41576be4f3fe8b5c9fd098700af43c1e8d9dcbf5
willjcimchamplain <72426716+willjcimchamplain@users.noreply.github.com>
2020-11-09 19:45:13 -0500

    Update week10.cpp

--- Week10/src/week10.cpp
+++ Week10/src/week10.cpp
@@ -13,6 +13,7 @@
 #include "util.h"
 
 main(){
+    // Initializes and populates a string vector
     vector <string> vectorStr;
     vectorStr.push_back("String One");
     vectorStr.push_back("String Two");
@@ -21,13 +22,15 @@ main(){
     vectorStr.push_back("String Five");
 
     cout << "Vector String Elements:\n";
-    for (int i = 0; i < MAX; i++){
+    for (int i = 0; i < MAX; i++){ // Outputs the strings in the vector
         cout << vectorStr[i] << endl;
     }
 
     cout << "A random element in this vector is: " << randomElement(vectorStr) << endl;
 
     cout << "Vector Double Elements:\n";
+    
+    // Initializes and populates a double vector
     vector <double> vectorDoub;
     for (double i = 0; i < MAX; i++){
         vectorDoub.push_back(i + i/10);
@@ -38,6 +41,7 @@ main(){
     cout << "The average of these doubles is: " << avg(vectorDoub) << endl;
     cout << "The lowest of these doubles is: " << lowest(vectorDoub) << endl;
 
+    // Initializes a multi word string
     string str = "A little bird with a yellow bill";
 
     cout << "A multi word string is: " << str << endl;
